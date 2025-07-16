cef29ecfe1d698880460f316adf05f5d1df8a859
Assaf Dori <assafdori@icloud.com>
2025-07-16 16:14:08 +0300

    feat: ✨ support helm & kubernetes providers

--- terraform/1-providers.tf
+++ terraform/1-providers.tf
@@ -1,3 +1,5 @@
+data "google_client_config" "default" {}
+
 provider "google" {
   project = local.project_id
   region  = local.region
@@ -11,5 +13,27 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.gke.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
   }
 }
