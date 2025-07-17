f6144b02726d16e6f99e291a6c333883bd600ad1
Assaf Dori <assafdori@icloud.com>
2025-07-17 17:30:27 +0300

    fix: 🐞 enable storage api

--- terraform/0-locals.tf
+++ terraform/0-locals.tf
@@ -3,6 +3,7 @@ locals {
   project_id = "sunlit-descent-465915-p7"
   region     = "us-central1"
   apis = [
+    "storage.googleapis.com",
     "compute.googleapis.com",
     "container.googleapis.com",
     "logging.googleapis.com",
