68234bc20cd1d4de8211c79807239dde3484aacc
Assaf Dori <assafdori@icloud.com>
2025-07-19 15:06:56 +0300

    fix: 🐞 debug cluster deletion

--- terraform/9-addons.tf
+++ terraform/9-addons.tf
@@ -1,27 +1,27 @@
-resource "kubernetes_namespace" "ingress_nginx" {
-  metadata {
-    name = "ingress-nginx"
-  }
-  depends_on = [google_container_cluster.gke]
-}
-
-resource "helm_release" "ingress_nginx" {
-  name       = "ingress-nginx"
-  namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.13"
-
-  values = [
-    yamlencode({
-      controller = {
-        service = {
-          annotations = {
-            "cloud.google.com/load-balancer-type" = "External"
-          }
-        }
-      }
-    })
-  ]
-  depends_on = [kubernetes_namespace.ingress_nginx]
-}
+# resource "kubernetes_namespace" "ingress_nginx" {
+#   metadata {
+#     name = "ingress-nginx"
+#   }
+#   # depends_on = [google_container_cluster.gke]
+# }
+#
+# resource "helm_release" "ingress_nginx" {
+#   name       = "ingress-nginx"
+#   namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
+#   repository = "https://kubernetes.github.io/ingress-nginx"
+#   chart      = "ingress-nginx"
+#   version    = "4.13"
+#
+#   values = [
+#     yamlencode({
+#       controller = {
+#         service = {
+#           annotations = {
+#             "cloud.google.com/load-balancer-type" = "External"
+#           }
+#         }
+#       }
+#     })
+#   ]
+#   depends_on = [kubernetes_namespace.ingress_nginx]
+# }
