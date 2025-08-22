6216c8bb1a7c9adbcc17ca06a533e6e178fbc98a
Assaf Dori <assafdori@icloud.com>
2025-08-22 12:30:47 +0300

    feat: ✨ bootstrap argocd

--- terraform/9-addons.tf
+++ terraform/9-addons.tf
@@ -1,27 +1,52 @@
-# resource "kubernetes_namespace" "ingress_nginx" {
-#   metadata {
-#     name = "ingress-nginx"
-#   }
-#   # depends_on = [google_container_cluster.gke]
-# }
-#
-# resource "helm_release" "ingress_nginx" {
-#   name       = "ingress-nginx"
-#   namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
-#   repository = "https://kubernetes.github.io/ingress-nginx"
-#   chart      = "ingress-nginx"
-#   version    = "4.13"
-#
-#   values = [
-#     yamlencode({
-#       controller = {
-#         service = {
-#           annotations = {
-#             "cloud.google.com/load-balancer-type" = "External"
-#           }
-#         }
-#       }
-#     })
-#   ]
-#   depends_on = [kubernetes_namespace.ingress_nginx]
-# }
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+  depends_on = [google_container_cluster.gke]
+}
+
+
+resource "helm_release" "argocd" {
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = false
+  verify           = false
+  wait             = true
+  version          = "8.3.0"
+
+  depends_on = [kubernetes_namespace.argocd]
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "kubernetes_manifest" "argocd_root_app" {
+
+  manifest = yamldecode(<<EOT
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: root-app
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: "https://github.com/assafdori/gcp-practice.git"
+    targetRevision: main
+    path: bootstrap
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: argocd
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+EOT
+  )
+  depends_on = [helm_release.argocd]
+}
+
